741c783cb37057d401b3f8abbf72be2c7904f4e9
Tendouji <tendoujir@gmail.com>
2017-10-22 23:03:52 +0800

    Dashboard v1 - fix removed component

--- src/app/shared/layout/pages/pages.component.ts
+++ src/app/shared/layout/pages/pages.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CodePreviewDirective } from '../../directives/code-preview/code-preview.directive';
 
 @Component({
   selector: 'dashboard-pages',
